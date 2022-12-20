e5ad3502c8df8095788ea5eed5895148919e187a
Kxffie <bryceriordan2005@gmail.com>
2022-12-19 22:41:45 -0600

    better ui

--- pages/index.tsx
+++ pages/index.tsx
@@ -3,8 +3,9 @@ import path from 'path';
 import matter from 'gray-matter';
 import Link from 'next/link';
 import { useState } from 'react';
-import { ChevronRight, ChevronLeft, Dice5 } from 'akar-icons';
 import Head from 'next/head';
+import { FiChevronLeft, FiChevronRight, FiChevronsLeft, FiChevronsRight, FiSearch } from 'react-icons/fi'
+import { GiPerspectiveDiceSixFacesRandom } from 'react-icons/gi'
 
 const Index = (props) => {
   const [searchQuery, setSearchQuery] = useState('');
@@ -35,7 +36,7 @@ const Index = (props) => {
       );
     }
 
-    const itemsPerPage = 10;
+    const itemsPerPage = 5;
     const startIndex = (page - 1) * itemsPerPage;
     const endIndex = startIndex + itemsPerPage;
     const currentPagePosts = filteredPosts.slice(startIndex, endIndex);
@@ -78,12 +79,26 @@ const Index = (props) => {
           </div >
         ))}
         <div className="flex justify-between mx-2 mt-6">
-          <button onClick={() => setPage(page - 1)} disabled={page === 1} className='px-2 py-1 text-lg font-semibold text-white rounded-md shadow-lg bg-violet-500 disabled:bg-gray-500 disabled:text-gray-400'>
-            <ChevronLeft size={36} />
-          </button>
-          <button onClick={() => setPage(page + 1)} disabled={page * itemsPerPage >= filteredPosts.length} className='px-2 py-1 ml-2 text-lg font-semibold text-white rounded-md shadow-lg bg-violet-500 disabled:bg-gray-500 disabled:text-gray-400'>
-            <ChevronRight size={36} />
-          </button>
+          <div>
+            {/* First Page */}
+            <button onClick={() => setPage(1)} disabled={page === 1} className='px-2 py-1 mr-3 text-lg font-semibold text-white rounded-md shadow-lg bg-violet-500 disabled:bg-gray-500 disabled:text-gray-400'>
+              <FiChevronsLeft size={36} />
+            </button>
+            {/* Last Page */}
+            <button onClick={() => setPage(page - 1)} disabled={page === 1} className='px-2 py-1 mr-3 text-lg font-semibold text-white rounded-md shadow-lg bg-violet-500 disabled:bg-gray-500 disabled:text-gray-400'>
+              <FiChevronLeft size={36} />
+            </button>
+          </div>
+          <div>
+            {/* Next Page */}
+            <button onClick={() => setPage(page + 1)} disabled={page * itemsPerPage >= filteredPosts.length} className='px-2 py-1 ml-3 text-lg font-semibold text-white rounded-md shadow-lg bg-violet-500 disabled:bg-gray-500 disabled:text-gray-400'>
+              <FiChevronRight size={36} />
+            </button>
+            {/* Last Page */}
+            <button onClick={() => setPage(Math.ceil(filteredPosts.length / itemsPerPage))} disabled={page * itemsPerPage >= filteredPosts.length} className='px-2 py-1 ml-3 text-lg font-semibold text-white rounded-md shadow-lg bg-violet-500 disabled:bg-gray-500 disabled:text-gray-400'>
+              <FiChevronsRight size={36} />
+            </button>
+          </div>
         </div>
       </div >
     );
@@ -102,8 +117,8 @@ const Index = (props) => {
       <br />
       <div className="flex">
         <input type="text" placeholder="Search tags..." value={searchQuery} onChange={handleSearchInput} className="flex-grow h-10 px-5 pr-16 text-sm bg-white border-2 border-gray-300 rounded-lg focus:border-violet-500 focus:outline-none" />
-        <button className='px-2 py-1 ml-2 text-lg font-semibold text-white rounded-md shadow-lg bg-violet-500' onClick={handleSearchSubmit}>Search</button>
-        <button className='px-2 py-1 ml-2 text-lg font-semibold text-white rounded-md shadow-lg bg-violet-500' onClick={goToRandomPost}><Dice5 size={30} /></button>
+        <button className='px-2 py-1 ml-2 text-lg font-semibold text-white rounded-md shadow-lg bg-violet-500' onClick={handleSearchSubmit}><FiSearch size={30} /></button>
+        <button className='px-2 py-1 ml-2 text-lg font-semibold text-white rounded-md shadow-lg bg-violet-500' onClick={goToRandomPost}><GiPerspectiveDiceSixFacesRandom size={30} /></button>
       </div>
       {handleSearchSubmit()}
     </div>
